4e8553921eae1f6d47911112b4665e8f653e2bc4
Metylalkohol <scorpion@profihost.sk>
2013-03-13 14:49:44 +0100

    testik

--- application/views/task_list.php
+++ application/views/task_list.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>Manažér úloh - administrácia v1.1 ahojkydasdas</title>
+        <title>Manažér úloh - administrácia v1.1 ahojkydasdasdadas</title>
         <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
