cb68297377a5a5dda0b4987fdea4049ca5a905f5
rtelicak <roman.telicak@gmail.com>
2013-03-30 18:44:14 +0100

    task detail design

--- application/views/task_detail.php
+++ application/views/task_detail.php
@@ -11,16 +11,66 @@
         </ul>
         <div class="container-fluid">
             <div class="row-fluid">
-                <!-- <div class="alert alert-error">
-                    <button type="button" class="close" data-dismiss="alert">×</button>
-                    Tento task je tesne pred deadlinom.
-                </div> -->
                 <div class="item-content">
-                    <div class="span9">
-                        <h2><?php echo $title; ?></h2>
-                        <p style="width: 700px;">
-                            <?php echo $desc; ?>
-                        </p>
+                    <div class="span9 offset1" style="padding-top:30px;"> 
+						<div>
+						<div class="row-fluid">
+							<div class="span1">
+								<h4>Nazov</h4>
+							</div>
+							<div class="span8 offset1">
+								<h4><?php echo $title; ?></h4>
+							</div>
+						</div>
+						<div class="row-fluid">
+							<div class="span1">
+								<h4>Popis</h4>
+							</div>
+							<div class="span9 offset1">
+								<p><?php echo $desc; ?></p>
+							</div>
+						</div> 
+						<div class="row-fluid">
+							<div class="span1">
+								<h4>Priradený</h4>
+							</div>
+							<div class="span8 offset1">
+								<p><?php echo $assigned_user; ?></p>
+							</div>
+						</div>
+						<div class="row-fluid">
+							<div class="span1">
+								<h4>Vytvorená</h4>
+							</div>
+							<div class="span8 offset1">
+								<p><?php echo $created; ?></p>
+							</div>
+						</div>
+						<div class="row-fluid">
+							<div class="span1">
+								<h4>Deadline</h4>
+							</div>
+							<div class="span8 offset1">
+								<p><?php echo $deadline; ?></p>
+							</div>
+						</div>
+						<div class="row-fluid">
+							<div class="span1">
+								<h4>Priorita</h4>
+							</div>
+							<div class="span8 offset1">
+								<p><span class="label label-important"><?php echo $priority; ?></span></p>
+							</div>
+						</div>
+						<div class="row-fluid">
+							<div class="span1">
+								<h4>Stav</h4>
+							</div>
+							<div class="span8 offset1">
+								<p><span class="label label-success"><?php echo $state; ?></span></p> 
+							</div>
+						</div>
+						</div>
                         <div class="mb-30" style="padding-top: 40px;">
                             <button class="btn btn-small btn-success"><i class="icon-ok"></i> Vyriešiť úlohu</button>
                             <button class="btn btn-small btn-primary"><i class="icon-bar-chart"></i> Začať pracovať na úlohe</button>
@@ -54,33 +104,16 @@
 							</div>
 						</div>
                     </div>
-                    <div class="span3">
-                        <div class="toc well boot">
-                            <h3 class="mt-0">Parametre úlohy</h3>
-                            <h4>Priradený:</h4>
-                            <p><?php echo $assigned_user; ?></p>
-                            <h4>Úloha pridaná</h4>
-                            <p><?php echo $created; ?></p>
-                            <h4>Deadline</h4>
-                            <p><?php echo $deadline; ?></p>
-                            <h4>Priorita</h4>
-                            <p><span class="label label-important"><?php echo $priority; ?></span></p>
-                            <h4>Stav</h4>
-                            <p><span class="label label-success"><?php echo $state; ?></span></p>
-                        </div>
-                    </div>
-                    <div class="clearfix">&nbsp;</div>
                 </div>
-                <footer>
-                    <hr>
-                    <p class="pull-right">
-                        Task manager v1.1
-                    </p>
-                    <div class="clearfix">&nbsp;</div>
-                </footer>
             </div>
         </div>
+        <footer>
+            <hr>
+            <p class="pull-right">Task manager v1.1</p>
+            <div class="clearfix">&nbsp;</div>
+        </footer>
     </div>
+
 	<script src="<?php echo base_url() ?>resources/bootstrap/js/bootstrap.js" type="text/javascript"></script>
     <script type="text/javascript">
         $("[rel=tooltip]").tooltip();
