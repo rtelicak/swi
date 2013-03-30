108831b911d4ca9d0121cf5fc763e7e2cdf81861
rtelicak <roman.telicak@gmail.com>
2013-03-30 17:06:40 +0100

    comment design

--- application/views/task_detail.php
+++ application/views/task_detail.php
@@ -28,33 +28,31 @@
 							<a href="<?php echo base_url() ?>task/edit_task/<?php echo $id_task ?>"><button class="btn btn-small btn-warning"><i class="icon-edit"></i> Upravit ulohu</button></a>
                         </div>
                         <h3>Komentáre</h3>
-                        <!-- <div class="comments-cover">  --> 
-							<ul class=comment-wrapper>
-								<?php foreach ($comments as $comment): ?>
-									<li class="comment">
-										<div class="comment-info">
-											<i class="icon-user"></i>&nbsp<?php echo $comment->user; ?></i><br/>
-											<small><?php echo $comment->dateTime; ?></small>
-											</div>
-										<div class="comment-bubble"><p><?php echo $comment->body; ?></p></div>
-									</li>
-								<?php endforeach ?>
-							</ul>
-                        <!-- </div>  -->
-						<!-- <div class="add-comment-wrapper">
+						<ul class=comment-wrapper>
+							<?php foreach ($comments as $comment): ?>
+								<li class="comment">
+									<div class="comment-info">
+										<i class="icon-user"></i>&nbsp<?php echo $comment->user; ?></i><br/>
+										<small><?php echo $comment->dateTime; ?></small>
+										</div>
+									<div class="comment-bubble"><p><?php echo $comment->body; ?></p></div>
+								</li>
+							<?php endforeach ?>
+						</ul>
+						<div class="add-comment-wrapper">
 							<div class="comment-box">
 	                        	<h4>Pridať nový komentár</h4>
 								<?php echo form_open('task/add_comment'); ?> 
 									<input type="hidden" name="id_user" id="id_user" value="<?php echo $id_logged_user ?>" />
 									<input type="hidden" name="id_task" id="id_task" value="<?php echo $id_task ?>" />
-		                        	<textarea name="body" id="commentarea" placeholder="Váš komentár.." rows="5" cols="25"></textarea>
+		                        	<textarea name="body" id="commentarea" placeholder="Váš komentár.." rows="5" cols="15"></textarea>
 		                            <div class="comment-btns">
 										<button type="submit" class="btn btn-small btn-success pull-left mr-10"><i class="icon-ok"></i> Pridať komentár</button>
 		    	                        <button type="button" class="btn btn-small btn-danger pull-left"><i class="icon-ban-circle"></i> Zmazať napísané</button>
 		                            </div>
 								</form>
 							</div>
-						</div> -->
+						</div>
                     </div>
                     <div class="span3">
                         <div class="toc well boot">
