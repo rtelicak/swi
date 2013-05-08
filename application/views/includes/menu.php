dde7387f839c75efa0f4496f4c12eda995854127
Metylalkohol <scorpion@profihost.sk>
2013-05-08 22:58:32 +0200

    menu changed

--- application/views/includes/menu.php
+++ application/views/includes/menu.php
@@ -6,12 +6,11 @@
     <ul id="dashboard-menu" class="nav nav-list collapse in">
         <li><a href="<?php echo base_url() ?>home">Domov</a></li>
         <li ><a href="<?php echo base_url() ?>statistics">Štatistiky</a></li>
-        <li ><a href="<?php echo base_url() ?>calendar">Kalendár úloh</a></li>
     </ul>
-    <a href="#tasks-menu" class="nav-header" data-toggle="collapse"><i class="icon-briefcase"></i>Úlohy<span class="label label-info">+3</span></a>
+    <a href="#tasks-menu" class="nav-header" data-toggle="collapse"><i class="icon-briefcase"></i>Úlohy<span class="label label-info"><i class="icon-th-list" style="margin:0 0.75em"></i></span></a>
     <ul id="tasks-menu" class="nav nav-list collapse">
-        <li ><a href="<?php echo base_url() ?>task/task_list">Prehľad úloh <span class="label label-info">+2</span></a></li>
-        <li ><a href="<?php echo base_url() ?>task/task_list/<?php $loggedUser = $this->session->userdata('logged_in');  echo $loggedUser['id']; ?>">Prehľad mojich úloh <span class="label label-info">+1</span></a></li>
+        <li ><a href="<?php echo base_url() ?>task/task_list">Prehľad úloh</a></li>
+        <li ><a href="<?php echo base_url() ?>task/task_list/<?php $loggedUser = $this->session->userdata('logged_in');  echo $loggedUser['id']; ?>">Prehľad mojich úloh</a></li>
         <li ><a href="<?php echo base_url() ?>task/add_task">Pridať novú úlohu</a></li>
     </ul>
    	<a href="<?php echo base_url() ?>user/user_list" class="nav-header" ><i class="icon-user"></i>Používatelia</a>
